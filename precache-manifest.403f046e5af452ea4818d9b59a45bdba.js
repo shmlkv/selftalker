a715e5b2a0343ffb6fe6080176cc49c06cb51aff
shmlkv <splo1t@icloud.com>
2019-03-28 21:42:20 +0300

    Updates

---
+++ precache-manifest.403f046e5af452ea4818d9b59a45bdba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "03d8d714971014ef28d0",
+    "url": "/static/css/main.f6c760bd.chunk.css"
+  },
+  {
+    "revision": "03d8d714971014ef28d0",
+    "url": "/static/js/main.9e38eba7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fc40805bdf6864ad6c04",
+    "url": "/static/js/2.07305811.chunk.js"
+  },
+  {
+    "revision": "c6da95b75a1a7b37d857013228e15976",
+    "url": "/static/media/chat.c6da95b7.jpg"
+  },
+  {
+    "revision": "dd57b49ac46e9760cb8a6dba80c6e03f",
+    "url": "/static/media/bg.dd57b49a.jpg"
+  },
+  {
+    "revision": "09e4cf44325aa090eb15e5900a12a504",
+    "url": "/index.html"
+  }
+];
